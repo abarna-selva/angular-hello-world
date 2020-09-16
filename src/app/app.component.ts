426020a21b356edb43d526ea5f48b5563118e04b
abarna-selva <abarna.rg@outlook.com>
2020-09-16 09:22:04 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,15 @@ export class AppComponent  {
   name = 'Abarna_Selva';
   title = "list of courses";
   imageurl = "https://loren.com/400/200";
+  // Event Binding:
+  onsave($event){
+    console.log("save button is clicked!",$event);
+  }
+  // Two way data binding:
+  // email = "abarna@example.com";
+  // onkeyup() {
+  //   console.log(this.email);
+  // }
+  // ngif:
+  course = [];
 }
